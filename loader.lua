1f826fef5762468aefb02e7628181cf82b3cbea0
heiderfaridf2012-ui <heiderfaridf2012@gmail.com>
2026-09-01 15:40:10 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,3 @@
--- GoldGuerrero-Hub Loader
+-- GoldGuerrero Hub - Loader Oficial
+-- No toques esto, solo carga el main.lua
 loadstring(game:HttpGet("https://raw.githubusercontent.com/heiderfaridf2012-ui/GoldGuerrero-Hub/main/main.lua"))()
